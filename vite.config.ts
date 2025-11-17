79b35bf680d4037cf7515d0086e85c35be6f182f
Karel <karelriyan@gmail.com>
2025-11-17 14:47:23 +0700

    register vite

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,12 @@ import { defineConfig } from 'vite';
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.tsx', 'resources/css/filament/admin/theme.css'],
+            input: [
+                'resources/css/app.css', 
+                'resources/js/app.tsx', 
+                'resources/css/filament/admin/theme.css',
+                'resources/css/filament/registration/theme.css'
+            ],
             ssr: 'resources/js/ssr.tsx',
             refresh: true,
         }),
